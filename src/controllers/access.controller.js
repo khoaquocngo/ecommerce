fc64bbff9c46aa3e25d5950cbaf66ff47ac560dd
khoaquocngo <nmkhoa17ck1@gmail.com>
2023-12-11 23:57:51 +0700

    Add Success Response

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -1,11 +1,19 @@
 'use strict'
 
+const { OK, CREATED } = require("../core/success.response");
 const AccessService = require("../services/access.service");
 
 class AccessController {
     signUp = async (req, res, next) => {
-        const result = await AccessService.signUp(req.body);
-        return res.status(201).json(result)
+        const metadata = await AccessService.signUp(req.body);
+
+        new CREATED({
+            message: 'Registered OK!',
+            metadata,
+            options: {
+                limit: 10,
+            }
+        }).send(res);
     }
 }
 
